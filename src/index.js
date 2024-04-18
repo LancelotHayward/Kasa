5114289b6aa2b6b359237b562213f2a76bafa709
LancelotHayward <hayward.lance@gmail.com>
2024-04-18 11:37:12 +0200

    Setup biome

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/';
-import About from './pages/About/';
-import FicheLogement from './pages/FicheLogement/';
-import Error from './pages/Error/';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/";
+import About from "./pages/About/";
+import FicheLogement from "./pages/FicheLogement/";
+import Error from "./pages/Error/";
 import KasaHeader from "./components/KasaHeader";
 import KasaFooter from "./components/KasaFooter";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-        <KasaHeader/>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/logement/*" element={<FicheLogement />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Error />} />
-        </Routes>
-        {/* <KasaFooter/> */}
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <KasaHeader />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/logement/*" element={<FicheLogement />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            {/* <KasaFooter/> */}
+        </Router>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
