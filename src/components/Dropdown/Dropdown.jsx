c08407a5e4f8d2c8de02b0fe9a5e5bf50122bc0b
LancelotHayward <hayward.lance@gmail.com>
2024-04-25 11:59:31 +0200

    Worked on dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,10 +1,24 @@
+import React, { useRef, useEffect } from 'react';
 import "./Dropdown.scss"
 
 function Dropdown({title, text}) {
+    const dropdown = useRef(null)
+    useEffect(() => {
+        dropdown.current.addEventListener('click', openDialog)
+    }, [])
+
+    // biome-ignore lint/correctness/noUnusedVariables: event is vital to functionality
+    const openDialog = (event) => {
+        document.getElementById(title).classList.toggle("open")
+    }
+
     return (
-        <article class="dropdown">
-            <h2>{title}</h2>
-            <p>{text}</p>
+        <article class="dropdown" ref={dropdown}>
+            <div>
+                <h2>{title}</h2>
+                <img src="dropdown-arrow.png" alt="Lire plus..."/>
+            </div>
+            <p id={title}>{text}</p>
         </article>
     )
 }
