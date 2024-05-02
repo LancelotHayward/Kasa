5a587c2d23f342024cc84ec4ae202b55846de7ea
LancelotHayward <hayward.lance@gmail.com>
2024-05-02 07:40:31 +0200

    Updated Gallery

--- src/components/Rental/Rental.jsx
+++ src/components/Rental/Rental.jsx
@@ -1,12 +1,13 @@
 import "./Rental.scss"
 import { Link } from 'react-router-dom'
 
-function Rental({link, title, background}) {
+function Rental({id, title, background}) {
+    const link = "/logement/" + id
     return (
-        <article className="rental">
-            <Link to={link}>{title}</Link>
+        <Link to={link} className="rental">
+            <p>{title}</p>
             <img src={background} alt={title}/>
-        </article>
+        </Link>
     )
 }
 export default Rental
