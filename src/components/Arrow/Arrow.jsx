85b7e357a432a894f56c7951d6e18dacd5229b30
LancelotHayward <hayward.lance@gmail.com>
2024-05-07 10:59:33 +0200

    Gallery Arrows

--- src/components/Arrow/Arrow.jsx
+++ src/components/Arrow/Arrow.jsx
@@ -1,14 +1,25 @@
 import { useRef, useEffect } from 'react';
 import "./Arrow.scss"
 
-function Arrow() {
-    function handleClick(side) {
-        if (side == "right") {
+function Arrow({side, photo, setPhoto, showPicture}) {
+    const arrowRef = useRef(null)
+    useEffect(() => {
+        arrowRef.current.addEventListener('click', handleClick)
+    }, [])
+    // biome-ignore lint/correctness/noUnusedVariables: event is vital to functionality
+    const handleClick = (event) => {
+        if (side === "right") {
             setPhoto(photo + 1)
-            showPicture(photo)
         }
+        showPicture(photo)
+    }
+    let alt = "précédent"
+    if (side === "right") {
+        alt = "suivant"
     }
     return (
-        
+        <img id={side} class="arrow" src="../gallery-arrow.png" alt={"Image " + alt} ref={arrowRef}/>
     )
 }
+
+export default Arrow
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react';
 import "./Gallery.scss"
 
+import Arrow from "../Arrow/Arrow.jsx"
+
 function Gallery({pictures, alt}) {
     const [photo, setPhoto] = useState(0);
     function showPicture(id) {
         const images = document.getElementsByClassName("picture")
-        images.forEach(image => {
-            // if (image.classList.contains("visible")) {
-                image.classList.remove("visible")
-            // }
-        })
+        for (let i = 0; i < images.length; i++){
+                images[i].classList.remove("visible")
+        }
         document.getElementById("rental-" + id).classList.add("visible")
     }
     return (
         <section id="Gallery">
+            <Arrow side="left" photo={photo} setPhoto={setPhoto} showPicture={showPicture}/>
             {pictures.map(function(picture, i){
                 let class_names = "picture"
                 if (i === 0) {
@@ -23,7 +24,8 @@ function Gallery({pictures, alt}) {
                     <img src={picture} alt={alt} id={"rental-" + i} className={class_names}/>
                 )
             })}
-            <div id="controls">1/4</div>
+            <div id="index">{photo + 1}/4</div>
+            <Arrow side="right" photo={photo} setPhoto={setPhoto} showPicture={showPicture}/>
         </section>
     )
 }
